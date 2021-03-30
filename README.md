4665e6ae192b1976c328b26c9c3d376125ad78b4
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-03-30 20:32:15 +0200

    created an App with JQuery to let the user add ,cross out , delete and reorderd items.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# to-do-list-app
+# to-do-list-app objective
+
+A basic to-do list app with the help of JQuery.
+
+**what should the app do?**
+
+The App should have some features:
+
+**$1** The user can add a new item to a list of items.
+
+**$2** The user can cross out an item from the list on doubleclick .
+
+**$3** The used can delete an item from the list of items when clicking on the 'X' button.
+
+**$4** The user can change the order of items in the list of items.
---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+function newItem () {
+
+
+//new item to the list
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.append(inputValue);
+
+if (inputValue === '') {
+    alert('You must write something!');
+} else {
+    let list = $('#list');
+    list.append(li);
+}
+
+//crossing out an item 
+/*function crossOut() {
+    li.addClass('strike');
+}*/
+li.on('dblclick', function(){
+    li.addClass('strike');
+});
+
+//Adding the delet button "X"
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
+
+crossOutButton.on('click', deleteListItem);
+
+//display none
+function deleteListItem(){
+    li.addClass('delete')
+}
+
+//Reordering items
+$('#list').sortable();
+
+}
+
